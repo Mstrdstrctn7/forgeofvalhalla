1c56e1653b906dfea9e27505bd989acac049a535
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-12 00:41:51 -0500

    Update supa.ts

--- src/lib/supa.ts
+++ src/lib/supa.ts
@@ -1,5 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-const url = import.meta.env.VITE_SUPABASE_URL as string;
-const key = import.meta.env.VITE_SUPABASE_KEY as string;
-export const supa = createClient(url, key);
+
+const url  = import.meta.env.VITE_SUPABASE_URL!;
+const anon = import.meta.env.VITE_SUPABASE_KEY!;
+
+export const supa = createClient(url, anon);
+
+// Also export as default so "import supa from" works
 export default supa;
