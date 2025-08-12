db10bd440aaf11d8ca78405ac18d2f3b31b36811
Mstrdstrctn7 <your@email.com>
2025-08-12 10:56:02 -0500

    chore(debug): stand-alone /_debug that bypasses app to avoid crash & help diagnose

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import "./devCrashGuard";
+import DebugStandalone from "./pages/DebugStandalone";
 import ErrorBoundary from "./components/ErrorBoundary";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -6,10 +7,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+function Root(){
+  const isDebug = typeof window !== "undefined" && window.location.pathname === "/_debug";
+  return isDebug ? <DebugStandalone/> : <Root/>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ErrorBoundary><App/></ErrorBoundary>
+      <ErrorBoundary><Root/></ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 )
