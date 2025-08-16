73b209c09337c07b4f66e74ebe305c072b43e9ba
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-16 18:02:16 -0500

    Update ColumnsTable.js

--- src/views/admin/dataTables/components/ColumnsTable.js
+++ src/views/admin/dataTables/components/ColumnsTable.js
@@ -1,8 +1,9 @@
+'use client';
 /* eslint-disable */
 
 import {
-  Flex,
   Box,
+  Flex,
   Table,
   Tbody,
   Td,
@@ -11,43 +12,45 @@ import {
   Thead,
   Tr,
   useColorModeValue,
-} from '@chakra-ui/react';
-import * as React from 'react';
-
+} from "@chakra-ui/react";
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   getSortedRowModel,
   useReactTable,
-} from '@tanstack/react-table';
-
-// Custom components
-import Card from 'components/card/Card';
-import Menu from 'components/menu/MainMenu';
+} from "@tanstack/react-table";
+import Card from "components/card/Card";
+import Menu from "components/menu/MainMenu";
+import * as React from "react";
 
 const columnHelper = createColumnHelper();
 
-// const columns = columnsDataCheck;
-export default function ColumnTable(props) {
+function HeaderText({ label }) {
+  return React.createElement(
+    Text,
+    {
+      justifyContent: "space-between",
+      align: "center",
+      fontSize: { sm: "10px", lg: "12px" },
+      color: "gray.400",
+    },
+    label
+  );
+}
+
+export default function CheckTable(props) {
   const { tableData } = props;
   const [sorting, setSorting] = React.useState([]);
-  const textColor = useColorModeValue('secondaryGray.900', 'white');
-  const borderColor = useColorModeValue('gray.200', 'whiteAlpha.100');
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
+
   let defaultData = tableData;
+
   const columns = [
-    columnHelper.accessor('name', {
-      id: 'name',
-      header: () => (
-        <Text
-          justifyContent="space-between"
-          align="center"
-          fontSize={{ sm: '10px', lg: '12px' }}
-          color="gray.400"
-        >
-          NAME
-        </Text>
-      ),
+    columnHelper.accessor("name", {
+      id: "name",
+      header: () => <HeaderText label="PROJECT" />,
       cell: (info) => (
         <Flex align="center">
           <Text color={textColor} fontSize="sm" fontWeight="700">
@@ -56,54 +59,9 @@ export default function ColumnTable(props) {
         </Flex>
       ),
     }),
-    columnHelper.accessor('progress', {
-      id: 'progress',
-      header: () => (
-        <Text
-          justifyContent="space-between"
-          align="center"
-          fontSize={{ sm: '10px', lg: '12px' }}
-          color="gray.400"
-        >
-          PROGRESS
-        </Text>
-      ),
-      cell: (info) => (
-        <Text color={textColor} fontSize="sm" fontWeight="700">
-          {info.getValue()}
-        </Text>
-      ),
-    }),
-    columnHelper.accessor('quantity', {
-      id: 'quantity',
-      header: () => (
-        <Text
-          justifyContent="space-between"
-          align="center"
-          fontSize={{ sm: '10px', lg: '12px' }}
-          color="gray.400"
-        >
-          QUANTITY
-        </Text>
-      ),
-      cell: (info) => (
-        <Text color={textColor} fontSize="sm" fontWeight="700">
-          {info.getValue()}
-        </Text>
-      ),
-    }),
-    columnHelper.accessor('date', {
-      id: 'date',
-      header: () => (
-        <Text
-          justifyContent="space-between"
-          align="center"
-          fontSize={{ sm: '10px', lg: '12px' }}
-          color="gray.400"
-        >
-          DATE
-        </Text>
-      ),
+    columnHelper.accessor("progress", {
+      id: "progress",
+      header: () => <HeaderText label="STATUS" />,
       cell: (info) => (
         <Text color={textColor} fontSize="sm" fontWeight="700">
           {info.getValue()}
@@ -111,7 +69,9 @@ export default function ColumnTable(props) {
       ),
     }),
   ];
+
   const [data, setData] = React.useState(() => [...defaultData]);
+
   const table = useReactTable({
     data,
     columns,
@@ -121,91 +81,63 @@ export default function ColumnTable(props) {
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
-    debugTable: true,
   });
+
   return (
-    <Card
-      flexDirection="column"
-      w="100%"
-      px="0px"
-      overflowX={{ sm: 'scroll', lg: 'hidden' }}
-    >
-      <Flex px="25px" mb="8px" justifyContent="space-between" align="center">
+    <Card extra="w-full h-full px-6 pb-6 sm:overflow-x-auto">
+      <Flex px="25px" justify="space-between" mb="20px" align="center">
         <Text
           color={textColor}
           fontSize="22px"
-          mb="4px"
           fontWeight="700"
           lineHeight="100%"
         >
-          4-Columns Table
+          Check Table
         </Text>
         <Menu />
       </Flex>
-      <Box>
+      <Box overflow={{ sm: "scroll", lg: "hidden" }}>
         <Table variant="simple" color="gray.500" mb="24px" mt="12px">
           <Thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <Tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <Th
-                      key={header.id}
-                      colSpan={header.colSpan}
-                      pe="10px"
-                      borderColor={borderColor}
-                      cursor="pointer"
-                      onClick={header.column.getToggleSortingHandler()}
-                    >
-                      <Flex
-                        justifyContent="space-between"
-                        align="center"
-                        fontSize={{ sm: '10px', lg: '12px' }}
-                        color="gray.400"
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                        {{
-                          asc: '',
-                          desc: '',
-                        }[header.column.getIsSorted()] ?? null}
-                      </Flex>
-                    </Th>
-                  );
-                })}
+                {headerGroup.headers.map((header) => (
+                  <Th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    pe="10px"
+                    borderColor={borderColor}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </Th>
+                ))}
               </Tr>
             ))}
           </Thead>
           <Tbody>
-            {table
-              .getRowModel()
-              .rows.slice(0, 11)
-              .map((row) => {
-                return (
-                  <Tr key={row.id}>
-                    {row.getVisibleCells().map((cell) => {
-                      return (
-                        <Td
-                          key={cell.id}
-                          fontSize={{ sm: '14px' }}
-                          minW={{ sm: '150px', md: '200px', lg: 'auto' }}
-                          borderColor="transparent"
-                        >
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext(),
-                          )}
-                        </Td>
-                      );
-                    })}
-                  </Tr>
-                );
-              })}
+            {table.getRowModel().rows.map((row) => (
+              <Tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <Td
+                    key={cell.id}
+                    fontSize={{ sm: "14px" }}
+                    minW={{ sm: "150px", md: "200px", lg: "auto" }}
+                    borderColor="transparent"
+                  >
+                    {flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext()
+                    )}
+                  </Td>
+                ))}
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </Box>
     </Card>
   );
-}
+            }
