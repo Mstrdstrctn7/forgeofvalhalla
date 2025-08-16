149bf1262ac95ce838a799b509fce31809afab94
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-16 16:25:20 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './assets/css/App.css';
-import routes from "../../routes.js";
 
 import App from './App';
 
